00ce8db7c91ebf8f9ba31fdf355a2c91822a2d9b
hmiranda3 <habibmiranda@gmail.com>
2016-05-10 09:54:23 -0600

    Final Stretch Product
    final commit

--- StretchProblem/FindNumbersDivisibleByX.playground/Contents.swift
+++ StretchProblem/FindNumbersDivisibleByX.playground/Contents.swift
@@ -3,3 +3,15 @@
 import UIKit
 
 var str = "Hello, playground"
+
+func divByX(x: Int, y: Int) {
+    
+    for int in 1...y { //int is a name I made up to call upon a particular int in the array)
+        if int % x == 0 {
+            print(int)
+        }
+    }
+}
+
+divByX(10, y: 100)
+
